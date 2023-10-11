d09291a9f5cb3e6ba4f59accfb38406f60ed97f3
johannes <johannes.muljana@ait.nsw.edu.au>
2023-10-11 18:16:59 +1100

    installed react navigation

--- App.js
+++ App.js
@@ -3,14 +3,22 @@ import { StyleSheet, Text, View } from 'react-native';
 // firebase
 import { firebaseConfig } from './config/Config';
 import { initializeApp } from 'firebase/app'
+// react navigation
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+// screens
+import { Signup } from './screens/Signup'
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   const FBapp = initializeApp( firebaseConfig )
   return (
-    <View style={styles.container}>
-      <Text>Hello world</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Sign up" component={Signup} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
