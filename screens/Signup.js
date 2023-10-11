244569a91afd42c2d94b665639316f71f6030abd
johannes <johannes.muljana@ait.nsw.edu.au>
2023-10-11 19:20:11 +1100

    auth context consumer

--- screens/Signup.js
+++ screens/Signup.js
@@ -1,15 +1,44 @@
 import { StyleSheet, Text, View, TextInput, Pressable } from 'react-native'
+import { useState, useEffect, useContext } from 'react'
+import { AuthContext } from '../contexts/AuthContext'
 
 export function Signup( props ) {
+  const[email,setEmail] = useState()
+  const[password,setPassword] = useState()
+
+  const auth = useContext(AuthContext)
+
+  // useEffect(() => { console.log(email) }, [email])
+  const submitHandler = () => {
+    props.handler( email, password )
+    .then( ( user ) => {
+      // sign up successful
+    })
+    .catch( (error) => {
+      console.log( error )
+    } )
+  }
+
   return(
     <View style={ styles.container }>
       <View style={ styles.form }>
         <Text style={ styles.title }>Register for an account</Text>
         <Text>Email</Text>
-        <TextInput style={styles.input} placeholder="you@example.com"/>
+        <TextInput 
+          style={styles.input} 
+          placeholder="you@example.com"
+          value={email}
+          onChangeText={(val) => setEmail(val)}
+        />
         <Text>Password</Text>
-        <TextInput style={styles.input} placeholder="minimum 8 characters"/>
-        <Pressable style={ styles.button }>
+        <TextInput 
+          style={styles.input} 
+          placeholder="minimum 8 characters"
+          secureTextEntry={true}
+          value={password}
+          onChangeText={(val) => setPassword(val)}
+        />
+        <Pressable style={ styles.button } onPress={() => submitHandler() }>
           <Text style={ styles.button.text }>Sign up</Text>
         </Pressable>
       </View>
