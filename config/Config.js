a2aff532ce2962859c4b886a321d7a0cd161f768
johannes <johannes.muljana@ait.nsw.edu.au>
2023-10-11 17:34:43 +1100

    firebase app config

---
+++ config/Config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyA3y5_si2VMTnMcfHTVJrvu4VbJVYoxy34",
+  authDomain: "expo23-d75c7.firebaseapp.com",
+  projectId: "expo23-d75c7",
+  storageBucket: "expo23-d75c7.appspot.com",
+  messagingSenderId: "800808212364",
+  appId: "1:800808212364:web:288e20fdf3001fe250a8a9"
+}
