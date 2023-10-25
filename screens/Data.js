c4ef2979085ad3f298f89db625df901c78d02b4d
johannes <johannes.muljana@ait.nsw.edu.au>
2023-10-25 19:32:46 +1100

    getting data in Data component

--- screens/Data.js
+++ screens/Data.js
@@ -1,8 +1,49 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, FlatList, StyleSheet } from 'react-native'
+import { useContext, useState, useEffect } from 'react'
+import { DbContext } from '../contexts/DbContext'
+import { AuthContext } from '../contexts/AuthContext'
+
+import { collection, getDocs } from "firebase/firestore"
+
 export function Data( props ) {
+  const db = useContext( DbContext )
+  const Auth = useContext( AuthContext )
+
+  const[ data, setData ] = useState([])
+  const[ user, setUser ] = useState()
+
+  const getData = async () => {
+    const col = collection( db, `things/${ user.uid }/list` )
+    const snapshot = await getDocs( col )
+    let dataList = []
+    snapshot.forEach( ( item ) => {
+      let obj = item.data()
+      obj.id = item.id
+      dataList.push( obj )
+    })
+    setData( dataList )
+    console.log( dataList )
+  }
+
+  useEffect( () => {
+    if( Auth.currentUser ) {
+      setUser( Auth.currentUser )
+      getData()
+    }
+    else {
+      setUser( null )
+    }
+  }, [ Auth ])
+
   return(
-    <View>
+    <View style={ styles.container }>
       <Text>Data</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 5,
+  }
+})
