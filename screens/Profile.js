bfb6940b777c2a4bfca0697b7d9ac419b4452017
johannes <johannes.muljana@ait.nsw.edu.au>
2023-10-25 17:58:00 +1100

    update to Profile component

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,8 +1,50 @@
 import { View, Text, Pressable, StyleSheet } from 'react-native'
+import { useContext, useState, useEffect } from 'react'
+
+import { AuthContext } from '../contexts/AuthContext'
+
 export function Profile( props ) {
-  return(
-    <View>
-      <Text>Profile</Text>
-    </View>
-  )
+  const [user,setUser] = useState()
+
+  const Auth = useContext( AuthContext )
+
+  useEffect( () => {
+    if( Auth.currentUser ) {
+      setUser( Auth.currentUser )
+    }
+  }, [Auth])
+
+  if( !user ) {
+    return(
+      <View style={ styles.container }>
+        <Text>Getting user data...</Text>
+      </View>
+    )
+  }
+  else {
+    return(
+      <View style={ styles.container }>
+        <Text>Hello {user.email}</Text>
+        <Pressable style={styles.button} >
+          <Text style={ styles.button.text }>Sign out</Text>
+        </Pressable>
+      </View>
+    )
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+  },
+  button: {
+    marginVertical: 15,
+    padding: 8,
+    backgroundColor: "#333333",
+    borderRadius: 6,
+    text: {
+      color: "white",
+      textAlign: "center",
+    }
+  }
+})
